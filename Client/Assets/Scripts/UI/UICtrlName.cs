5b19dd5c350ccf8880381b8c583fef12ff96a66c
Mofwf <958237724@qq.com>
2024-03-02 20:52:53 +0800

    角色model

--- Client/Assets/Scripts/UI/UICtrlName.cs
+++ Client/Assets/Scripts/UI/UICtrlName.cs
@@ -4,4 +4,5 @@ public class UICtrlName
     /// 测试UI
     /// </summary>
     public const string TestUIPrefab = "Assets/UIPrefab/test_panel.prefab";
+    public const string JieMianUIPrefab = "Assets/UIPrefab/JieMianUI.prefab";
 }
--- Client/Assets/Scripts/UI/UIguanli.cs
+++ Client/Assets/Scripts/UI/UIguanli.cs
@@ -1,18 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityGameFramework.Runtime;
 
-public class UIguanli : MonoBehaviour
+public class UIguanli : UIFormLogic
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    [SerializeField]
+    private Button _btnShuaxin;
+
+    [SerializeField]
+    private Button _btnShouqi;
+
+    [SerializeField]
+    private Canvas _cavJiemian;
+    protected override void OnInit(object userData)
     {
-        
+        transform.position = Vector3.zero;
+        _btnShuaxin.onClick.AddListener(OnClickBtnShuaxin);
+        _btnShouqi.onClick.AddListener(OnClickBtnShouqi);
+        base.OnInit(userData);
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnClickBtnShouqi()
     {
-        
+        _cavJiemian.transform.SetPositionY(-_cavJiemian.transform.position.y);
+        Log.Info("TestBtn OnClick");
+    }
+    private void OnClickBtnShuaxin()
+    {
+        Log.Info("TestBtn OnClick");
     }
 }
